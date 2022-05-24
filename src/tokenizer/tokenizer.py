4cf3336287b854028931884096073251bf6ae9c8
Thomas Mondor <thomas.mondor.1@ulaval.ca>
2022-05-23 23:03:42 -0400

    added Tokenizer and Token classes draft and TokenType enum

--- src/tokenizer/tokenizer.py
+++ src/tokenizer/tokenizer.py
@@ -1,3 +1,21 @@
-# Tokenizer/Lexer class for the language.
+import tokens
 
-# Relies on the Token class hierarchy.
+
+class Tokenizer:
+    """Tokenizer/Lexer class for the language.
+        Relies on the Token class.
+    """
+    def __init__(self, input_string : str):
+        self.raw_program = input_string
+        self.tokenized_program = []
+
+    def tokenize(self):
+        lines = self.raw_program.split('\n')
+        for line_nbr, line in enumerate(iterable)(lines):
+            raw_tokens = line.split(' ')
+            for raw_token in raw_tokens:
+                token = self.get_token(raw_token, line_nbr)
+                self.tokenized_program.append(token)
+
+    def get_token(self, raw_token : str, line_nbr : int):
+        return tokens.Token(raw_token, line_nbr)
