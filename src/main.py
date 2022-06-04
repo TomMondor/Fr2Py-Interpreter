3039ee0cc5a3197cde59f20676dd7f79117a09cd
Thomas Mondor <thomas.mondor.1@ulaval.ca>
2022-06-04 15:43:51 -0400

    Updated main to demonstrate Tokenizer class usage

--- src/main.py
+++ src/main.py
@@ -1,3 +1,34 @@
-# main program
+import tokenizer.tokenizer as tk
 
-# command line input : file path containing the program to run.
+"""
+Main program
+Will eventually offer a command line input asking for the file path containing the program to run.
+A GUI could also be provided in the future.
+"""
+
+sample_code = """
+a = "Hello World"
+b = 123.43
+c = a + b # invalid operation
+abc = "dskl#fsj" # le caractère " est permis dans des commentaires
+x = demande("Quel est ton chiffre favori")
+si x < 3
+alors
+    affiche(x, "est plus petit que 3")
+sinon
+    affiche(x, "is greater than 3")
+fin-si
+maVariable1 = 2 + 3 * 4 # ceci est un commentaire, this is a comment
+maVariable2 = maVariable1 >= 17
+maVariable3 = 3 / 2 #maVariable3 = 1.5
+maVariable4 = "du texte..." + " some text"
+
+fonction maFonction1(x, y, z)
+    retourne x + y + z
+"""
+
+the_tokenizer = tk.Tokenizer(sample_code)
+tokenized_program = the_tokenizer.tokenize()
+# print(the_tokenizer)
+the_tokenizer.print_brief()
+# print(tokenized_program)
