5be1fad20d10e427f0f5538d29cf4ce8fda90eef
Thomas Mondor <thomas.mondor.1@ulaval.ca>
2022-06-04 15:39:45 -0400

    Updated Token and TokenType classes

--- src/tokenizer/tokens.py
+++ src/tokenizer/tokens.py
@@ -2,21 +2,31 @@ import enum
 
 
 class Token:
-    def __init__(value : str, line_nbr : int):
-        self.type = self.get_token_type(value)
+    def __init__(self, line_nbr : int, tokenType: 'TokenType', value: str = None):
+        self.type = tokenType
         self.value = value
         self.line_nbr = line_nbr
 
     def __str__(self):
-        return f'Token({self.type}, {self.value})'
+        return f"Token[{self.type.name}: '{self.value}']"
 
-    def get_token_type(self, value : str):
-        #TODO return a TokenType (create exception class if no match)
-        pass
-
+    def __repr__(self):
+        return self.__str__()
 
 
 class TokenType(enum.Enum):
+    # Keywords.
+    PRINT = 'affiche'
+    INPUT = 'demande'
+    RETURN = 'retourne'
+    FUNCTION = 'fonction'
+    ELSE = 'sinon'
+    END_IF = 'fin-si'
+    THEN = 'alors'
+    IF = 'si'
+    OR = 'ou'
+    AND = 'et'
+
     # Single-character tokens.
     LEFT_PAREN = '('
     RIGHT_PAREN = ')'
@@ -29,14 +39,14 @@ class TokenType(enum.Enum):
     QUOTE_MARK = '"'
     COMMENT = '#'
 
-    # One or two character tokens.
+    # Logical operators tokens.
     NOT_EQUAL = 'pas='
     NOT = 'pas'
-    EQUAL = '='
-    GREATER = '>'
     GREATER_EQUAL = '>='
-    LESS = '<'
+    GREATER = '>'
     LESS_EQUAL = '<='
+    LESS = '<'
+    EQUAL = '='
 
     # Literals.
     STRING = 'texte'
@@ -45,17 +55,33 @@ class TokenType(enum.Enum):
     FUNCTION_NAME = 'FUNCTION_NAME'
     VARIABLE_NAME = 'VARIABLE_NAME'
 
-    # Keywords.
-    ELSE = 'sinon'
-    END_IF = 'fin-si'
-    THEN = 'alors'
-    IF = 'si'
-    OR = 'ou'
-    AND = 'et'
-    PRINT = 'affiche'
-    RETURN = 'retourne'
-    FUNCTION = 'fonction'
-
     # End markers
     EOL = '\n'
     EOF = 'EOF'
+
+    NOT_A_TOKEN = 'NOT_A_TOKEN'
+
+    def get_token_name(self):
+        return self.name
+
+    def get_token_value(self):
+        return self.value
+
+    @classmethod
+    def get_token_type_from_value(cls, value):
+        for token in cls:
+            if token.value == value:
+                return token
+        return TokenType.NOT_A_TOKEN
+
+    @classmethod
+    def is_valid_token_type(cls, value):
+        return cls.get_token_type_from_value(value) != TokenType.NOT_A_TOKEN
+
+    @classmethod
+    def get_non_litteral_tokens(cls):
+        return [token for token in cls if token.value not in ['\n', 'EOF', 'texte', 'nombre', 'vrai-ou-faux', 'FUNCTION_NAME', 'VARIABLE_NAME', 'NOT_A_TOKEN']]
+
+    @classmethod
+    def get_non_litteral_tokens_values(cls):
+        return [token.value for token in cls.get_non_litteral_tokens()]
