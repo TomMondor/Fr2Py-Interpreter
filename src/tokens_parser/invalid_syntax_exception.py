97bb0cfe0751e65cafeef819b494ec8e9badab30
Thomas Mondor <thomas.mondor.1@ulaval.ca>
2022-06-07 21:44:21 -0400

    Updated InvalidSyntaxException to handle printing of Tokens

--- src/tokens_parser/invalid_syntax_exception.py
+++ src/tokens_parser/invalid_syntax_exception.py
@@ -1,7 +1,24 @@
+from typing import Union
+from tokenizer.tokens import Token
+
+
+
 class InvalidSyntaxException(Exception):
-    def __init__(self, value : str, line_nbr : int):
+    def __init__(self, value : Union[str, list[Token]], line_nbr : int):
         self.value = value
         self.line_nbr = line_nbr
 
     def __str__(self):
-        return f"InvalidSyntaxException: Invalid syntax '{self.value}' at line {self.line_nbr}"
+        #TODO make debugging easier by passing the raw program and printing the raw line of code
+        if isinstance(self.value, str):
+            return f"InvalidSyntaxException: Invalid syntax '{self.value}' at line {self.line_nbr}"
+        elif isinstance(self.value, list):
+            output = ""
+            for token in self.value:
+                output += str(token)
+                output += " "
+            output += "\n"
+            output += f"InvalidSyntaxException: Invalid syntax at line {self.line_nbr}"
+            return output
+        else:
+            return f"InvalidSyntaxException: Invalid syntax at line {self.line_nbr}"
