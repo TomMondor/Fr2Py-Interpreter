df56b48ac57f4032ae6604b91159ba31803c541c
Thomas Mondor <thomas.mondor.1@ulaval.ca>
2022-06-18 17:46:11 -0400

    Updated main to use and test the Interpreter

--- src/main.py
+++ src/main.py
@@ -1,5 +1,7 @@
 from tokenizer.tokenizer import Tokenizer
 from tokens_parser.parser import Parser
+from interpreter.interpreter import Interpreter
+from interpreter.storage import Storage
 
 """
 Main program
@@ -12,7 +14,8 @@ TODO: line numbers in the error messages might be wrong, because the tokenizing 
 sample_code = """
 a = "Hello World"
 b = 123.43
-c = a + b # invalid operation
+#c = a + b # invalid operation
+affiche(a, "  wowwwww")
 abc = "le caractère # est permis dans les strings" # et le caractère " est permis dans des commentaires
 x = demande("Quel est ton chiffre favori")
 si x < 3
@@ -30,13 +33,14 @@ fonction maFonction1(x, y, z)
     retourne x + y + z
 
 somme = maFonction1(1.5, b, 32)
+affiche("somme devrait être '156.93' et est :", somme)
 
 #bug = maFonction1 "syntaxe invalide" #bel et bien détecté par le parser
 """
 
-the_tokenizer = Tokenizer(sample_code)
-tokenized_program = the_tokenizer.tokenize()
+tokenized_program = Tokenizer(sample_code).tokenize()
 
-the_parser = Parser(tokenized_program)
-ast = the_parser.parse()
-print(str(ast))
+ast = Parser(tokenized_program).parse()
+# print(str(ast))
+
+Interpreter(ast, Storage()).interpret()
